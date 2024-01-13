0c4dd0b86b283f6d482685f6757baf7993b98644
Igor Bastos <114706936+IgorBastos19@users.noreply.github.com>
2024-01-13 18:04:37 +0000

    adicionar pagina principal

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home () {
+    return <h1>Estou com você, o nosso desafio é a conquista do Mundo, vamos atrás do nosso sonho! Cada passo que damos estamos mais de completar esse desafio! 2/193 e contando. Thamyres sabe que eu te amo né e se você me ama da uma Risadinha!!</h1>
+}
+
+export default Home;
