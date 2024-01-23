02ba3b4b6add88e5cf5596bb55e6700d6f878394
Igor Bastos <114706936+IgorBastos19@users.noreply.github.com>
2024-01-23 12:10:22 +0000

    adiciona scripts `link:check` e `link:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clonte-tabnews
+
 Projeto para aprender a programar, o conteudo é sobre as aulas do curso.dev do Filipe, começando em Janeiro de 2024
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-function Home () {
-    return <h1>Estou com você, o nosso desafio é a conquista do Mundo, vamos atrás do nosso sonho! Cada passo que damos estamos mais de completar esse desafio! 2/193 e contando. Thamyres sabe que eu te amo né e se você me ama da uma Risadinha!!</h1>
+function Home() {
+  return (
+    <h1>
+      Estou com você, o nosso desafio é a conquista do Mundo, vamos atrás do
+      nosso sonho! Cada passo que damos estamos mais de completar esse desafio!
+      2/193 e contando. Thamyres sabe que eu te amo né e se você me ama da uma
+      Risadinha!!
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
